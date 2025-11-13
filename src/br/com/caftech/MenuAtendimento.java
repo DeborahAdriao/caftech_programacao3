3651db9fb538a121ce3d4c8583825725b2a5e55f
mikhelli <raianymikhelli@gmail.com>
2025-11-12 23:51:59 -0300

    criação de menu

--- src/br/com/caftech/MenuAtendimento.java
+++ src/br/com/caftech/MenuAtendimento.java
@@ -1,10 +1,11 @@
-
 package br.com.caftech;
+
 
 import br.com.caftech.decorator.Bebida;
 import br.com.caftech.decorator.adicionais.*;
 import br.com.caftech.factory.*;
 import br.com.caftech.singleton.CaixaFinanceiroSingleton;
+import br.com.caftech.singleton.Pedido;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -12,65 +13,96 @@ public class MenuAtendimento {
 
     private Scanner scanner;
     private CafeteriaFactory factory;
+    //inicio do singleton
+    private CaixaFinanceiroSingleton caixa;
 
     public MenuAtendimento() {
         this.scanner = new Scanner(System.in);
+        this.caixa = CaixaFinanceiroSingleton.getInstancia();
     }
 
-    // metodo principal do menu att
+    //inicio do menu
     public void iniciar() {
-        System.out.println("Seja Bem Vindo ao CoffeTech");
+        System.out.println("Seja bem vindo a cafeteria CoffeTech");
         System.out.println("---------------------------------");
 
-        try {
-            // inicializa  a fabrica dos factories
-            Bebida bebidaDoCliente = escolherBebidaBase();
+        boolean executando = true;
+        while (executando) {
+            System.out.println("\n--- MENU PRINCIPAL ---");
+            System.out.println("1. Registrar Novo Pedido");
+            System.out.println("2. Listar Histórico de Vendas");
+            System.out.println("0. Sair do Sistema");
+            System.out.print("Escolha uma opção: ");
 
-            // começa a diconar os decoradores
-            bebidaDoCliente = adicionarExtras(bebidaDoCliente);
-
-            // finaliza o pedido
-            System.out.println("\n=================================");
-            System.out.println("PEDIDO FINALIZADO:");
-            System.out.println("Descrição: " + bebidaDoCliente.getDescricao());
-
-            // formatação de preco
-            System.out.printf("Custo Total: R$ %.2f\n", bebidaDoCliente.getCusto());
-            System.out.println("=================================");
-
-            // singleton para registar a venda
-            double valorFinal = bebidaDoCliente.getCusto();
-            CaixaFinanceiroSingleton.getInstancia().registrarVenda(valorFinal);
-
-            System.out.println("\nVenda registrada no caixa!");
-            System.out.printf("Total atual do caixa: R$ %.2f\n",
-                    CaixaFinanceiroSingleton.getInstancia().getSaldoTotal());
-
-            System.out.println("\nObrigado por comprar conosco!");
-
-        } catch (Exception e) {
-            System.err.println("Ocorreu um erro inesperado no sistema: " + e.getMessage());
-        } finally {
-            // Fecha o scanner ao final do programa
-            if (scanner != null) {
-                scanner.close();
+            try {
+                int escolha = scanner.nextInt();
+                switch (escolha) {
+                    case 1:
+                        // mini menu p criar o pedido
+                        fazerNovoPedido();
+                        break;
+                    case 2:
+                        // vai listar o historico de pedidos
+                        caixa.listarHistorico();
+                        break;
+                    case 0:
+                        executando = false;
+                        break;
+                    default:
+                        System.out.println("Você escolheu uma opção inválida. Tente novamente.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Ainda não entendi.... por favor digite uma opção válida");
+                scanner.next();
             }
         }
+
+        System.out.println("\nSistema encerrado. Obrigado e volte sempre!");
+        scanner.close();
     }
 
-    // mini menu para escolher a bebida
+    // mini menu de criação de pedido
+    private void fazerNovoPedido() {
+        System.out.println("\n--- NOVO PEDIDO ---");
+
+        // escolha da base de bebida com o factory
+        Bebida bebidaDoCliente = escolherBebidaBase();
+
+        // escolha dos acicionais do decorator
+        bebidaDoCliente = adicionarExtras(bebidaDoCliente);
+
+        System.out.println("\n--- Método de Pagamento ---");
+        System.out.printf("(Valor base do pedido: R$ %.2f)\n", bebidaDoCliente.getCusto());
+        System.out.print("Digite o método (Ex: Dinheiro, Cartão, Pix): ");
+
+        scanner.nextLine();
+        String metodoPag = scanner.nextLine();
+
+        // registro do pedido no singleton
+        System.out.println("\n[Sistema] Registrando seu pedido no caixa...");
+
+        Pedido novoPedido = new Pedido(
+                bebidaDoCliente.getDescricao(),
+                bebidaDoCliente.getCusto(),
+                metodoPag
+        );
+        // registro do novo pedido e já lança o saldo atualizado no caixa
+        caixa.registrarPedido(novoPedido);
+    }
+
+
+    // escolha da base de bebida
     private Bebida escolherBebidaBase() {
-        System.out.println("--- Escolha a base do seu café ---");
+        System.out.println("--- Por favor escolha sua Bebida Base! ---");
         System.out.println("1: Café Expresso");
         System.out.println("2: Cappuccino");
         System.out.println("3: Latte");
-        System.out.println("4: Café Gourmet");
+        System.out.println("4. Café Gourmet");
 
         while (true) {
             System.out.print("Digite a opção (1-4): ");
             try {
                 int escolha = scanner.nextInt();
-
                 switch (escolha) {
                     case 1:
                         this.factory = new CafeExpressoFactory();
@@ -85,25 +117,21 @@ public class MenuAtendimento {
                         this.factory = new CafeGourmetFactory();
                         return factory.criarBebida();
                     default:
-                        // trata o erro de opção invalida
-                        System.out.println("Opção inválida. Por favor, escolha um número entre 1 e 4.");
+                        System.out.println("Não entendi... por favor digite uma opção válida");
                 }
             } catch (InputMismatchException e) {
-                // Trata erro de digitação do pedido da base
-                System.out.println("Ainda não consigo entende.. por favor digite apenas números");
-                scanner.next();
+                System.out.println("Ainda não entendi... por favor digite os números válidos.");
+                scanner.next(); // trata erro caso digite string ao invés de escolher num
             }
         }
     }
 
-    // mini menu dos decoradores do café
+
     private Bebida adicionarExtras(Bebida bebidaBase) {
         Bebida bebidaAtual = bebidaBase;
-
+        System.out.println("Quer deixar seu pedido mais saboroso? Escolha algum adicional! Caso esteja satisfeito digite 0");
         while (true) {
-            System.out.println("\n--- Adicionar Extras ---");
-            // Mostra o pedido atual
-            System.out.printf("(Seu pedido: %s | Custo: R$ %.2f)\n",
+            System.out.printf("\n(Pedido atual: %s | Custo: R$ %.2f Para finalizar digite 0)\n",
                     bebidaAtual.getDescricao(), bebidaAtual.getCusto());
 
             System.out.println("1: Baunilha");
@@ -115,7 +143,6 @@ public class MenuAtendimento {
 
             try {
                 int escolha = scanner.nextInt();
-
                 switch (escolha) {
                     case 1:
                         bebidaAtual = new Baunilha(bebidaAtual);
@@ -134,15 +161,12 @@ public class MenuAtendimento {
                         System.out.println("Chocolate adicionado!");
                         break;
                     case 0:
-                        // retorna após inserir os decoradores
                         return bebidaAtual;
                     default:
-                        // trata erro caso escolha númeor que não está na opção
-                        System.out.println("Opção inválida. Por favor, escolha um número entre 0 e 4.");
+                        System.out.println("Opção inválida.");
                 }
             } catch (InputMismatchException e) {
-                // Trata erro de digitação
-                System.out.println("Ainda não consigo entende.. por favor digite apenas números");
+                System.out.println("Não entendi... por favor digite os números válidos.");
                 scanner.next();
             }
         }
--- src/br/com/caftech/singleton/Pedido.java
+++ src/br/com/caftech/singleton/Pedido.java
@@ -1,17 +1,21 @@
 package br.com.caftech.singleton;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
-
+// tive que deixar as classes pedido publicas para poder implementar ele no menu
 public class Pedido implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String descricaoBebida;
     private double valorTotal;
     private String metodoPagamento;
     private LocalDateTime dataHora;
 
-    private Pedido(String descricaoBebida, double valorBase, String metodoPagamento){
+    public Pedido(String descricaoBebida, double valorBase, String metodoPagamento){
         this.descricaoBebida = descricaoBebida;
         this.metodoPagamento = metodoPagamento;
         this.dataHora = LocalDateTime.now();
+
 
         if (metodoPagamento.equalsIgnoreCase("cartao") || metodoPagamento.equalsIgnoreCase("cartão")){
             this.valorTotal = valorBase * 2.00;
@@ -23,6 +27,8 @@ public class Pedido implements Serializable {
     public double getValorTotal(){
         return valorTotal;
     }
+
+    // metodo para mostrar o historico
     @Override
     public String toString(){
         return String.format("[%s] %s - Método: %s - Total: R$ %.2f",
@@ -32,3 +38,4 @@ public class Pedido implements Serializable {
                 valorTotal);
     }
 }
+
